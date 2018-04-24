7789542ab2bb3969f1bb1febdc00d5bc19ff62f0
Sam Lambrick <34570926+slambrick@users.noreply.github.com>
2018-04-24 12:31:15 +0100

    Update makeEffuse.m

--- functions/makeEffuse.m
+++ functions/makeEffuse.m
@@ -1,10 +1,10 @@
+function [effuse_pos, effuse_dir, n_effusive] = makeEffuse(n_rays, ...
+        effuse_size, pinhole_c)
 % Copyright (c) 2018, Sam Lambrick.
 % All rights reserved.
 % This file is part of the SHeM Ray Tracing Simulation, subject to the 
 % GNU/GPL-3.0-or-later.
-
-function [effuse_pos, effuse_dir, n_effusive] = makeEffuse(n_rays, ...
-        effuse_size, pinhole_c)
+%
 % Generates the effuse beam for use in the SHeM simulation. Assumes the effuse
 % beam follows a normallty centred cosiune distribution centred on the middle of
 % the pinhole.
